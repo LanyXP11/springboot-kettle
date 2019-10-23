1a77f54f6fb7427e2edc3dc5aafc75e1a6ae0c81
chenjiang <chenjiang@hetrone.com>
2019-10-23 18:37:25 +0800

    添加文件上传配置

--- src/main/java/com/lx/kettle/SpringbootKettleApplication.java
+++ src/main/java/com/lx/kettle/SpringbootKettleApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 
 /**
@@ -23,5 +26,19 @@ public class SpringbootKettleApplication extends SpringBootServletInitializer {
         System.err.println("启动成功");
     }
 
+    /**
+     * 添加文件上传配置
+     *
+     * @return
+     */
+    @Bean(name = "multipartResolver")
+    public MultipartResolver multipartResolver() {
+        CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+        resolver.setDefaultEncoding("UTF-8");
+        resolver.setResolveLazily(true);
+        resolver.setMaxInMemorySize(40960);
+        resolver.setMaxUploadSize(1024000000);
+        return resolver;
+    }
 
 }
