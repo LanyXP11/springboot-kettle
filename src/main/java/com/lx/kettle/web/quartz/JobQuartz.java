436615bb601050d5cf75de22c2da7e8f273cca7b
chenjiang <chenjiang@hetrone.com>
2019-10-24 18:31:56 +0800

    添加注释

--- src/main/java/com/lx/kettle/web/quartz/JobQuartz.java
+++ src/main/java/com/lx/kettle/web/quartz/JobQuartz.java
@@ -6,7 +6,8 @@ import org.quartz.*;
 /**
  * Created by chenjiang on 2019/10/24
  * <p>
- *     @DisallowConcurrentExecution该注解的作用
+ *     @DisallowConcurrentExecution 该注解的作用不允许并发执行多个Job
+ *     这里的并发作用于jobDetail 如一个JOB类下面有多个jobDetail的时候添加次注解不会并执行jobDetail
  * </p>
  */
 @DisallowConcurrentExecution
@@ -18,6 +19,11 @@ public class JobQuartz implements InterruptableJob {
 
     }
 
+    /**
+     * 
+     * @param context
+     * @throws JobExecutionException
+     */
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
 
