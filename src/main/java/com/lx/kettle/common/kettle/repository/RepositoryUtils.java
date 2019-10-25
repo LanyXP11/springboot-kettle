f0fe2c3e4ccc720455aabd716be317dd4d614aa7
chenjiang <chenjiang@hetrone.com>
2019-10-25 21:52:01 +0800

    请求修改

---
+++ src/main/java/com/lx/kettle/common/kettle/repository/RepositoryUtils.java
@@ -0,0 +1,11 @@
+package com.lx.kettle.common.kettle.repository;
+
+/**
+ * Created by chenjiang on 2019/10/25
+ * <p>
+ *     该工具类主要用于的是Kettle链接资源库的全局通用工具类
+ * </p>
+ */
+public class RepositoryUtils {
+
+}
--- src/main/java/com/lx/kettle/web/quartz/JobQuartz.java
+++ src/main/java/com/lx/kettle/web/quartz/JobQuartz.java
@@ -1,6 +1,8 @@
 package com.lx.kettle.web.quartz;
 
+import com.alibaba.fastjson.JSON;
 import com.lx.kettle.common.tootik.Constant;
+import com.lx.kettle.core.model.KRepository;
 import com.lx.kettle.web.quartz.model.DBConnectionModel;
 import lombok.extern.slf4j.Slf4j;
 import org.pentaho.di.trans.Trans;
@@ -36,25 +38,58 @@ public class JobQuartz implements InterruptableJob {
      */
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
+
         JobDataMap transDataMap = context.getJobDetail().getJobDataMap();
-        //获取文件资源库对象   和    数据资源库对象
-        Object KRepositoryObject = transDataMap.get(Constant.REPOSITORYOBJECT);
-        Object DbConnectionObject = transDataMap.get(Constant.DBCONNECTIONOBJECT);
+        JobDataMap jobDataMap = context.getJobDetail().getJobDataMap();
 
-        String transId = String.valueOf(transDataMap.get(Constant.TRANSID));//转换ID
-        String transPath = String.valueOf(transDataMap.get(Constant.TRANSPATH));//路径地址
-        String transName = String.valueOf(transDataMap.get(Constant.TRANSNAME));//转换名称
-        String userId = String.valueOf(transDataMap.get(Constant.USERID));//用户ID
-        String logLevel = String.valueOf(transDataMap.get(Constant.LOGLEVEL));//日志级别
-        String logFilePath = String.valueOf(transDataMap.get(Constant.LOGFILEPATH));//日志存放路径地址
+        Object KRepositoryObject = jobDataMap.get(Constant.REPOSITORYOBJECT);
+        Object DbConnectionObject = jobDataMap.get(Constant.DBCONNECTIONOBJECT);
 
+        String jobName_str = context.getJobDetail().getKey().getName();
+        String[] names = jobName_str.split("@");
+
+        String jobId = String.valueOf(jobDataMap.get(Constant.JOBID));
+        String jobPath = String.valueOf(jobDataMap.get(Constant.JOBPATH));
+        String jobName = String.valueOf(jobDataMap.get(Constant.JOBNAME));
+        String userId = String.valueOf(jobDataMap.get(Constant.USERID));
+        String logLevel = String.valueOf(jobDataMap.get(Constant.LOGLEVEL));
+        String logFilePath = String.valueOf(jobDataMap.get(Constant.LOGFILEPATH));
         Date lastExecuteTime = context.getFireTime();
         Date nexExecuteTime = context.getNextFireTime();
+
         //判断数据库连接对象是否正确
         if (DbConnectionObject != null && DbConnectionObject instanceof DBConnectionModel) {
+            //证明是从资源库中获取的
+            if (KRepositoryObject != null && KRepositoryObject instanceof KRepository) {
+                try {
+                    runRepositoryJob(KRepositoryObject,DbConnectionObject,jobId,jobPath,jobName,userId,logLevel,logFilePath,lastExecuteTime,nexExecuteTime);
+                } catch (Exception e) {
+                    log.error("运行资源库DB:{},出现异常,异常详情msg:{}", JSON.toJSONString(KRepositoryObject),e);
+                }
+            }else{
+
+            }
 
         }
 
 
+    }
+
+    /**
+     * 运行资源库的作业
+     *
+     * @param KRepositoryObject 数据库连接对象
+     * @param DbConnectionObject 资源库对象
+     * @param jobId             作业ID
+     * @param jobPath          作业在资源库中的路径信息
+     * @param jobName          作业名称
+     * @param userId           作业归属者ID
+     * @param logLevel         作业的日志等级
+     * @param logFilePath      作业日志保存的根路径
+     * @param executeTime
+     * @param nexExecuteTim
+     */
+    private void runRepositoryJob(Object KRepositoryObject, Object DbConnectionObject, String jobId,
+                                  String jobPath, String jobName, String userId, String logLevel, String logFilePath, Date executeTime, Date nexExecuteTim) {
     }
 }
