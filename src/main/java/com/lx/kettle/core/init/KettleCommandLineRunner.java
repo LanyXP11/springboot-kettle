92fecf6dcc8121e301493936551240eefdc45cc9
LanyXP11 <lanyxp@163.com>
2019-12-04 21:50:33 +0800

    加载kettle环境

--- src/main/java/com/lx/kettle/core/init/KettleCommandLineRunner.java
+++ src/main/java/com/lx/kettle/core/init/KettleCommandLineRunner.java
@@ -20,10 +20,10 @@ public class KettleCommandLineRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         long startLog = System.currentTimeMillis();
-        log.info("初始化kettle环境开始");
-        KettleEnvironment.init();
         log.info("初始化自定义kettle环境开始");
-//        KettleInit.init();//TODO
+        KettleInit.environmentInit();//TODO
+        log.info("初始化kettle环境开始");
+        org.pentaho.di.core.KettleEnvironment.init();
         log.info("初始化kettle环境结束 总耗时:{}:毫秒", (System.currentTimeMillis() - startLog));
     }
 }
